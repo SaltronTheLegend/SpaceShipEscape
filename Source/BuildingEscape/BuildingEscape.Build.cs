5a64f0d0c29d7083e4de0c013b87ee83800ba5d7
nobody157 <116177645+nobody157@users.noreply.github.com>
2022-10-19 19:02:48 +0530

    Update BuildingEscape.Build.cs

--- Source/BuildingEscape/BuildingEscape.Build.cs
+++ Source/BuildingEscape/BuildingEscape.Build.cs
@@ -11,6 +11,11 @@ public class BuildingEscape : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		//PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		//blicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+
+		//PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
